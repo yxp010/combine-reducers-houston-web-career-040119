352c16e98a7710392b59482464dc7e7fd423e6ab
PYN <yxp7531540@gmail.com>
2019-06-22 15:05:34 -0500

    done

---
+++ src/reducers/authorsReducer.js
@@ -0,0 +1,24 @@
+import uuid from "uuid";
+
+export default function authorsReducer(state = [], action) {
+    switch(action.type) {
+      case "ADD_AUTHOR":
+        return [...state.authors, action.author];
+    
+      case "REMOVE_AUTHOR":
+        idx = state.indexOf(action.id);
+        return [...state.slice(0, idx), ...state.slice(idx + 1)];
+      case "ADD_BOOK":
+        let existingAuthor = state.filter(
+          author => author.authorName === action.book.authorName
+        );
+        if (existingAuthor.length > 0) {
+          return state;
+        } else {
+          return [...state, { authorName: action.book.authorName, id: uuid() }];
+        }
+      default:
+        return state;
+      }
+  }
+  
